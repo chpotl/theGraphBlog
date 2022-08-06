c4f6569f28f8013518c78b45c40a4b36fc5ecc74
Leonid Zotov <zotovleonid@icloud.com>
2022-08-06 19:53:07 +0300

    bugfix: createpost

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,113 @@
-import "./App.css";
-import { ethers } from "ethers";
-import { useState } from "react";
+import './App.css';
+import { ethers } from 'ethers';
+import { useState } from 'react';
+import { contractAddress, ownerAddress } from './config';
+import Blog from './Blog.json';
 
 function App() {
   const { ethereum } = window;
-  const [posts, updatePosts] = useState([""]);
+  const [posts, updatePosts] = useState([]);
   const [showPosts, setShowPosts] = useState(false);
-  const [title, setTitle] = useState("");
-
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, Blog.abi, signer);
+  let address;
   async function connect() {
-    console.log("connect");
-    // let address;
+    if (ethereum) {
+      try {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        if (ethereum.selectedAddress) {
+          address = ethereum.selectedAddress;
+          console.log(address);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  }
 
-    // if (ethereum) {
-    //   try {
-    //     await ethereum.request({ method: "eth_requestAccounts" });
-    //     if (ethereum.selectedAddress) {
-    //       address = ethereum.selectedAddress;
-    //       connect.innerText = address;
-    //     }
-    //   } catch (err) {
-    //     console.error(err);
-    //   }
-    // }
+  async function fetchPosts() {
+    try {
+      const res = await contract.fetchPosts();
+      res.forEach((el) => {
+        console.log(el.title, el.content, el.author);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function fetchPost(id) {
+    try {
+      const res = await contract.fetchPost(id);
+      console.log(res.title, res.content, res.author);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function createPost() {
+    console.log('create post');
+
+    try {
+      const provider = new ethers.providers.JsonRpcProvider(
+        'http://127.0.0.1:8545/'
+      );
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(contractAddress, Blog.abi, signer);
+      const val = await contract.createPost(title, content);
+
+      console.log('trx: ', val);
+    } catch (err) {
+      console.log('err: ', err);
+    }
   }
 
   function renderPosts(posts) {
-    return posts.map((post) => <p key={post}>{post}</p>);
-  }
-
-  function createPost() {
-    console.log("create post");
-    updatePosts([...posts, title]);
-
-    // const provider = new ethers.providers.Web3Provider(window.ethereum);
-    // const signer = provider.getSigner();
-    // const contract = new ethers.Contract(contractAddress, Blog.abi, signer);
+    return posts.map((post) => {
+      return (
+        <div key={post.title} className='post-item'>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      );
+    });
   }
 
   return (
-    <div className="App">
-      <button onClick={() => connect()} className="connect-btn">
+    <div className='App'>
+      <button onClick={() => connect()} className='connect-btn'>
         Connect to wallet
       </button>
-      <button onClick={() => setShowPosts(!showPosts)} className="getposts-btn">
+      <button onClick={() => fetchPosts()} className='getposts-btn'>
         Get posts
       </button>
-      {showPosts ? (
-        <div className="posts-block">{renderPosts(posts)}</div>
-      ) : (
-        <div className="posts-block">you have no posts yet. add smth</div>
-      )}
+      <button onClick={() => fetchPost(1)} className='getposts-btn'>
+        Fetch post
+      </button>
+
       <input
         onChange={(e) => {
           setTitle(e.target.value);
         }}
-        className="title-input"
-        placeholder="title"
+        className='title-input'
+        placeholder='title'
       />
-      <textarea className="text-area" />
-      <button onClick={() => createPost()} className="createpost-btn">
+      <textarea
+        onChange={(e) => setContent(e.target.value)}
+        className='text-area'
+      />
+      <button onClick={() => createPost()} className='createpost-btn'>
         Create Post
       </button>
+
+      {showPosts ? (
+        <div className='posts-block'>{renderPosts(posts)}</div>
+      ) : (
+        'no posts yet'
+      )}
     </div>
   );
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,7 @@ async function main() {
   /* this code writes the contract addresses to a local */
   /* file named config.js that we can use in the app */
   fs.writeFileSync(
-    './config.js',
+    './client/src/config.js',
     `
     export const contractAddress = "${blog.address}"
     export const ownerAddress = "${blog.signer.address}"
